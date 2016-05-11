be208e2b63a1174ff889c22f25fb8169144a3be1
Mikou <michael.jespersen@gmail.com>
2016-05-11 10:08:12 +0200

    inputSteam + tokenizer

---
+++ code/readNext.js
@@ -0,0 +1,24 @@
+function readNext() {
+    readWhile(isWhitespace);
+    if (input.eof()) return null;
+    var ch = input.peek();
+    // COMMENT (skip it)
+    if (ch === "'") {
+      skipComment();
+      return readNext();
+    }
+    // DATETIME
+    if (ch === "#")     return readDatetime();
+    // STRING
+    if (ch === '"')     return readString();
+    // NUMBER
+    if (isDigit(ch))    return readNumber();
+    // IDENT OR PATH
+    if (isIdStart(ch))  return readIdentOrPath();
+    // OPERATOR
+    if(isOpChar(ch))    return readOperator();
+    // EOL
+    if (ch === "\n")    return { type:"EOL", value: input.next() }
+
+    input.croak("Can't handle character: " + ch);
+}
